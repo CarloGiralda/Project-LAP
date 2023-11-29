3d5428e393339f145077ffc4d29fdb7e9622333a
CarloGiralda <92364122+CarloGiralda@users.noreply.github.com>
2023-11-29 19:15:21 +0100

    Delete CarSearch/src directory

--- CarSearch/src/main/java/com/example/CarSearch/service/SearchService.java
+++
@@ -1,9 +0,0 @@
-package com.example.CarSearch.service;
-
-import com.example.CarSearch.model.ResponseDTO;
-import com.example.CarSearch.model.SearchDTO;
-import org.springframework.web.bind.annotation.RequestBody;
-
-public interface SearchService {
-    public ResponseDTO getCar(SearchDTO dto);
-}
