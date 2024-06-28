7e1812d959d863761d0a8f90807a1f02719a862e
GabrieleLerani <gabriele.lerani2000@gmail.com>
2024-06-28 14:29:08 +0200

    Solved some issues

--- blockchain (node 1)/src/main/java/LAP/Blockchain/service/BlockchainService.java
+++ blockchain (node 1)/src/main/java/LAP/Blockchain/service/BlockchainService.java
@@ -3,6 +3,7 @@ package LAP.Blockchain.service;
 import LAP.Blockchain.dto.Block;
 import LAP.Blockchain.dto.Payment;
 import LAP.Blockchain.model.*;
+import LAP.Blockchain.util.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.util.encoders.Base64;
 import org.springframework.context.annotation.PropertySource;
@@ -31,43 +32,8 @@ public class BlockchainService {
         this.blockchain = blockchain;
         this.producerService = producerService;
         this.actualBlock = new Block();
-        this.public_key = "-----BEGIN PUBLIC KEY-----" +
-                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzAzDBCwejYVjX7Vi3ZK3" +
-                "Y0urENRSGqV+Wok1IUbg56bovq5VVJ0Kj0bEf6BGKyBOjzsyFFO6CGsBbSkWMuRs" +
-                "GlLkfYFdduv2u0+TRYFCeTz7EOuIky3Wt0b9NqzQQ5z0zojFJCbDcPVoTvY5pEAc" +
-                "qwakVM/kFURcxHTMcvstNHHYGdTocX/2Do9WAXYYARIjYGLyItyU0zbhDMVKw6GP" +
-                "WSzUzlyBYPibU2X3xfJLHBf+/w3k8b2fS7C3rdjT1Dilt6SYt3xttr4Uc1q0eWVA" +
-                "vtvPazynNJMOSUyXa0pmPdZ/G34aBkh+o/4fGrB4zE7+4X+t0gNDcBRZ85pOv4sX" +
-                "swIDAQAB" +
-                "-----END PUBLIC KEY-----";
-        this.private_key = "-----BEGIN PRIVATE KEY-----" +
-                "MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDMDMMELB6NhWNf" +
-                "tWLdkrdjS6sQ1FIapX5aiTUhRuDnpui+rlVUnQqPRsR/oEYrIE6POzIUU7oIawFt" +
-                "KRYy5GwaUuR9gV126/a7T5NFgUJ5PPsQ64iTLda3Rv02rNBDnPTOiMUkJsNw9WhO" +
-                "9jmkQByrBqRUz+QVRFzEdMxy+y00cdgZ1Ohxf/YOj1YBdhgBEiNgYvIi3JTTNuEM" +
-                "xUrDoY9ZLNTOXIFg+JtTZffF8kscF/7/DeTxvZ9LsLet2NPUOKW3pJi3fG22vhRz" +
-                "WrR5ZUC+289rPKc0kw5JTJdrSmY91n8bfhoGSH6j/h8asHjMTv7hf63SA0NwFFnz" +
-                "mk6/ixezAgMBAAECggEBAIolcbrXa/AAiOD5WvAMLuCpEC4asWOc+8ir8C6RYB81" +
-                "34mJWfgpQkbycIK/rgHwo39RYnxkmySr3ZFcKc1W3OGNkWhvNyoWYzs5ismZ07ll" +
-                "Uc2IyTRr6ly3USO9KFs+XneeHWp2XAeT/oTHKwtK5J3dp1BMV20WlX/kW67U4ywH" +
-                "We5qVyy3Syv2QLbWp4uHk4f+7nAvx1/PvFxcpPS0WkNtEi0gXBms7Wp/dwF/Mdyt" +
-                "sz/m1xnWM7opzKzu/SrPWBXsjoroaQEnDWDAYXu8Yifs1se1pn7nlw5yKUaaoS51" +
-                "4xblInBPLMKpLmVOYEFH7AY2t0UDK8bDW0wNi24UH8ECgYEA6VQf5IilzQqdrb+K" +
-                "OZcNHALR5Rl8Z127QDfaj46q78bmwQ7euwnzCTmSuFmhGC8j+GO+oOFDGsjak+9i" +
-                "vRuWkkb4n1c3D+WgFmFHjCsEodZYuQdBoKfrKxFyPWg4wm+Ni5ybqhNysvUYYbx8" +
-                "zBO4fC3w5ziA8p9eYJHw/H+VSWMCgYEA3+BZjpJT3by0F05CszW3iTtH1NVpsEJz" +
-                "GYVe2wBh9OIGn+pDtpyTLM/dUw64dN+jrYlyPF0dg13YGLJV29w6nuWqUkFLPZEk" +
-                "jibbylc6lnWtr8Skni8+lHNUxiGObWom7J0az44cTmnG9U7Nzts6XIpBEqsghuCd" +
-                "9eB/MbYLcXECgYEAqgJp2pLWfDgn+9l8VcoB+07ysQOI6agaMAiCb+d8FYnPyQuU" +
-                "rqPpOeeBSGHGR2DRzMKgu4hm5LVEzlW2lCPt6ldcAH4D2Mlvg98NysvtbB5KN0da" +
-                "Qj0X1SG5I5U1BjdR41AQ/DN/d3Wudkhohio4WgL/bPp2ulH06lhsRw6PMBECgYBi" +
-                "wsbfNUAYDnnn2hdklWrc+DDM/ER+hCHgfUyGEhokfF1tyFuMwG/QkfZSAKlwrYO4" +
-                "N20UogM2A30kD+/+aJ3XoPtNBA7cHx5gM1Y8YdSvsUQoyPfR7IeP13zrVrxNGkoH" +
-                "kZrVOxzTnqgtaVEzUtThJABm6uUTex1T7XBPDr7nkQKBgQC/qGK3qK2sfGxIgAS/" +
-                "T/hZ1C4ckn/9mlXAmDCd2B8DeE4uY5PZKm3Gb/Q06avCcrpYAL++/MNb/6AXaCVW" +
-                "RI7dSnQgruFPiUF6bb6i7HUfi75HuOARc5NtIQn66W0TSsCdCVOzCrtcGa3k5lJr" +
-                "wi6NhgM1e4Kz97jOmcwJa7UvqA==" +
-                "-----END PRIVATE KEY-----";
+        this.public_key = Constants.publicKey;
+        this.private_key = Constants.privateKey;
     }
 
     public int addTransaction(Payment payment) {
--- blockchain (node 2)/src/main/java/LAP/Blockchain/service/BlockchainService.java
+++ blockchain (node 2)/src/main/java/LAP/Blockchain/service/BlockchainService.java
@@ -3,6 +3,7 @@ package LAP.Blockchain.service;
 import LAP.Blockchain.dto.Block;
 import LAP.Blockchain.dto.Payment;
 import LAP.Blockchain.model.*;
+import LAP.Blockchain.util.Constants;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -47,43 +48,8 @@ public class BlockchainService {
         this.producerService = producerService;
         this.start = true;
         this.actualBlock = new Block();
-        this.public_key = "-----BEGIN PUBLIC KEY-----" +
-                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArxolxdsPX3hFX9tunQnW" +
-                "QimGFY0YISMqFhQ7Ol5bgg0Lknx6+BJLnbiuMjdLHq7FHQqn/1IQs+AHYVGMZrPp" +
-                "pKOQFSLTt2eMGTPHuRlqS4cGJYxL7mKegZEltTQIzkaBQplgYXOqvF3njYuHYF9S" +
-                "pUpEFKXK6rvCowxkAmrqmYGUb1q6RdHtl8qrJ0mJvRTiFWdYcWV2UJdnavkQRK0y" +
-                "sbcwBOUh8w9+Htl0asmQ+Z+CWI/ttdDdM8DFXCxWl15b8gF1e9+Tuae8gKNPYK8b" +
-                "pLrMsM2i1lxN+1653YysnBjW767v4mLIMFNlQEi1KyBdSy2q+Nat96mcjKbRdHzX" +
-                "eQIDAQAB" +
-                "-----END PUBLIC KEY-----";
-        this.private_key = "-----BEGIN PRIVATE KEY-----" +
-                "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCvGiXF2w9feEVf" +
-                "226dCdZCKYYVjRghIyoWFDs6XluCDQuSfHr4EkuduK4yN0sersUdCqf/UhCz4Adh" +
-                "UYxms+mko5AVItO3Z4wZM8e5GWpLhwYljEvuYp6BkSW1NAjORoFCmWBhc6q8XeeN" +
-                "i4dgX1KlSkQUpcrqu8KjDGQCauqZgZRvWrpF0e2XyqsnSYm9FOIVZ1hxZXZQl2dq" +
-                "+RBErTKxtzAE5SHzD34e2XRqyZD5n4JYj+210N0zwMVcLFaXXlvyAXV735O5p7yA" +
-                "o09grxukusywzaLWXE37XrndjKycGNbvru/iYsgwU2VASLUrIF1LLar41q33qZyM" +
-                "ptF0fNd5AgMBAAECggEAOBK8g+bJvn3qYW8IthtfVKKtgJtFXd8/7AAgk2mCDlyV" +
-                "Fxzr2CMrud9ZUVVppfK7xrgR4m5qUN/uHRkbSAxXRtTmIA6eE/6B29Q8IidkZblg" +
-                "m4eZDUbhkYZkt+lo5H2piSVKuwpqY5YCv84t1QvRkIL+2EnbeF4WT1Z5dFi+tddE" +
-                "iK0ayY40SCyRvGJAcmt7zn1JL5C/8wWsVm1RXrLrZAITg52KCG2AyICrnYakn5E5" +
-                "fGt22oYlIDn0GMevNbx6EV7XJhwbIBskGtagsUNT9yZaCar6AbAuTIFs1tvnxZRQ" +
-                "Xt6xAtxRzkla0/vG28unItv4kQ2L5/JwP85xPrDOgQKBgQDYygLQXhM2kCcTUSkv" +
-                "3vm0DgTEqYRdok4Cclu2pBNHg4knZRf0fAX3zteT3r15a/6U1Qd3nGmQnZYZE9Dq" +
-                "60Y6nuZ7ofNXK1d5WOBO5K91+Z9wUtV4phHJq5U5b9CHCabjtjfjiQYwDIqsmxw9" +
-                "lYz/NOpHDwDpDCio9fowdmJyiQKBgQDOxeeD8jLkFbFnAh/zWOFSYRm10KmjTU4G" +
-                "eMiVF6/i39nASy7gOjxWzReyrhnZfLMaKbUXL8AUp3NIrUMJb7fEpVh425QgmG9E" +
-                "YzfaPrDrFN9CQ1dG26vL2D/z18mnjF6UiohJFqiqTrT7xE7Rlieo50ZvQd3n4rqo" +
-                "chvAlXfBcQKBgQC+AHds5jbSqI8G2ChQA444p9y3/Ja9dgYHEPGe5rjbAcbQl0Li" +
-                "jVyxr0C0IhFQfT1YdkkgJR9El0k5W2vMqT4ZHLGHBA8BhEQbX7DglH/P12NND9x2" +
-                "7J5EcbTM7VoHMIdrpoXfT3Xz4Nodo6ihtRQpjNgURWsX1gdA5q1LkkaAeQKBgBbp" +
-                "exP13InFlZjSdXPjn2QfelfZPVLpGAJDlAZhT44WO+zA5/rUuSbtJvkXramZ1cbK" +
-                "ZFSjZ+dh/rZI7Kq5BGX35GvP+QP7oCi1zdSTKcuCzpJrH9qsMMcvuTZt7NxyA3D2" +
-                "QM/IgPwOKKPVRt6WguKGbhmZ9NrjFsffjQ+0AEchAoGAd5bseGH4pUzIIMG4exYH" +
-                "rFnygGFOJnl+QeyM2FLv4U9v3qxI5ffJKYbhCfyM9hKRL7AG7fjxxHsSgeNJc19d" +
-                "Zk2o2nqLbwhQWzN8u0aIhnD4/hFAWjPneb6WODnaEvXpCiSuGvcxi8yPtnNS8S8Z" +
-                "v3vWtF8IVzI7M+BIOr9ArG0=" +
-                "-----END PRIVATE KEY-----";
+        this.public_key = Constants.publicKey;
+        this.private_key = Constants.privateKey;
     }
 
     public int addTransaction(Payment payment) {
@@ -122,7 +88,7 @@ public class BlockchainService {
     }
 
     public ArrayList<Block> retrieveBlockchain() throws JsonProcessingException {
-        /*
+
         String blockchainUrl = discoveryClientService.getServiceUrl("BLOCKCHAIN-SERVICE");
         String blockchainRetrieveUrl = blockchainUrl + "/blockchain/getBlockchain";
 
@@ -135,11 +101,11 @@ public class BlockchainService {
         }
 
         return blocks;
-        */
 
-        List<String> nodeIPs = discoveryClientService.getServiceInstances("BLOCKCHAIN-SERVICE");
+
+        /*List<String> nodeIPs = discoveryClientService.getServiceInstances("BLOCKCHAIN-SERVICE");
         SocketClient socketClient = new SocketClient(nodeIPs.get(0), 5001);
-        SocketServer socketServer = new SocketServer(5001);
+        SocketServer socketServer = new SocketServer(5001);*/
         // TODO after this, the connection happens on this socket
     }
 
